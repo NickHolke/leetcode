45aec8e2f79b776a972ae3bd59457697fe29beee
Nick Holke <nickholke@gmail.com>
2020-01-30 10:30:36 -0800

    Refactor

--- pivotIndex.js
+++ pivotIndex.js
@@ -12,19 +12,15 @@ you should return the left-most pivot index.
 var pivotIndex = function(nums) {
   if (nums.length <= 2) return -1;
   
-  if (nums.slice(1).reduce((a,b) => a+b) === 0) {
-      return 0;
-  }
-  
-  for (let i = 1; i < nums.length - 1; i++) {
-    if (nums.slice(0,i).reduce((a,b) => a + b) === nums.slice(i+1).reduce((a,b) => a+b)) {
-      return i;
-    }
-  }
+  let sum = nums.reduce((a,b) => a +b);
+  let leftSum = 0;
     
-  if (nums.slice(0, nums.length - 1).reduce((a,b) => a+b) === 0) {
-      return nums.length - 1;
+  for (let i = 0; i < nums.length; i++) {
+      if (leftSum === sum - nums[i] - leftSum) {
+          return i;
+      } else {
+          leftSum+=nums[i];
+      }
   }
-    
   return -1;
 };
