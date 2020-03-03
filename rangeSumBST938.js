919cfd064d02bd2733c751e32557513bdd978653
Nick Holke <nickholke@gmail.com>
2020-03-03 09:29:16 -0800

    refactored with recursion

--- rangeSumBST938.js
+++ rangeSumBST938.js
@@ -24,3 +24,48 @@ var rangeSumBST = function(root, L, R) {
   
   return sum;
 };
+
+const rangeSumBST = (root, L, R) => {
+  let total = 0;
+  
+  const innerRec = (curr, L, R) => {
+      if (curr === null) {
+          return;
+      }
+      
+      if (curr.val >= L && curr.val <= R) {
+          total += curr.val;
+          innerRec(curr.left, L, R);
+          innerRec(curr.right, L, R);
+      }
+      
+      if (curr.val < L) {
+          innerRec(curr.right, L, R);
+      }
+      
+      if (curr.val > R) {
+          innerRec(curr.left, L, R);
+      }
+  }
+  
+  innerRec(root, L, R);
+  return total;
+}
+
+const rangeSumBST = (root, L, R) => {
+  if (root === null) {
+      return 0;
+  }
+
+  if (root.val >= L && root.val <= R) {
+      return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R);
+  }
+
+  if (root.val < L) {
+      return rangeSumBST(root.right, L, R);
+  }
+
+  if (root.val > R) {
+      return rangeSumBST(root.left, L, R);
+  }
+}
