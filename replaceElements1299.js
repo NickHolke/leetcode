a00e4ec06d4b1f572d074c27a6aa2d169e082420
Nick Holke <nickholke@gmail.com>
2020-03-05 09:09:11 -0800

    refactored

--- replaceElements1299.js
+++ replaceElements1299.js
@@ -7,3 +7,14 @@ var replaceElements = function(arr) {
   arr[arr.length - 1] = -1;
   return arr;
 };
+
+const replaceElements = (arr) => {
+  let max = -1, curr;
+  for (let i = arr.length - 1; i >= 0; i--) {
+      curr = arr[i];
+      arr[i] = max;
+      max = Math.max(curr, max);
+  }
+  
+  return arr;
+}
