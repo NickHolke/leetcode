36a54c63bb5bcb6dcd78267cb3480ca935b172ed
Nick Holke <nickholke@gmail.com>
2020-03-18 10:01:29 -0700

    refactored

--- topKfrequentElements347.js
+++ topKfrequentElements347.js
@@ -23,3 +23,25 @@ var topKFrequent = function(nums, k) {
   
   return results;
 };
+
+const topKFrequent = (nums, k) => {
+  let count = {}, buckets = [], result = [];
+  nums.forEach((num) => {
+      count[num] = count[num] + 1 || 1;
+  })
+  for (let num in count) {
+      let idx = count[num];
+      if (buckets[idx] === undefined) {
+          buckets[idx] = [];
+      }
+      buckets[idx].push(num);
+  }
+  
+  for (let i = buckets.length - 1; i >= 0 && result.length < k; i--) {
+      if (buckets[i] === undefined) {
+          continue;
+      }
+      result = result.concat(buckets[i]);
+  }
+  return result.slice(0,k);
+}
