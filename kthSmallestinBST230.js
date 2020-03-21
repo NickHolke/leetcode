7cb7d7113cc3063de47242b433918d0606b2573d
Nick Holke <nickholke@gmail.com>
2020-03-21 09:16:05 -0700

    iterative

--- kthSmallestinBST230.js
+++ kthSmallestinBST230.js
@@ -17,3 +17,25 @@ var kthSmallest = function(root, k) {
   return result;
   
 };
+
+const kthSmallest = (root, k) => {
+  let stack = [];
+  while (root !== null) {
+      stack.push(root);
+      root = root.left;
+  }
+  
+  while (k !== 0) {
+      let curr = stack.pop();
+      k--;
+      if (k === 0) {
+          return curr.val;
+      }
+      
+      let right = curr.right;
+      while (right !== null) {
+          stack.push(right);
+          right = right.left;
+      }
+  }
+}
